09d1eb3a95b79c29c95259c5182c8087c759d0af
zhangqingchun4 <zhangqingchun4@xdf.cn>
2020-10-28 10:44:19 +0800

    修改配置

--- build.sh
+++ build.sh
@@ -6,25 +6,18 @@ echo "image_name is null"
 exit 1
 fi
 date=`date +%Y%m%d%H%M%S`
-docker build -t $image_name:$date .
+docker build -t registry.cn-hangzhou.aliyuncs.com/ceres-spring/$image_name:$date .
 if [ $? -eq 0 ]
 then
 echo "build success"
-docker tag $image_name:$date registry.cn-hangzhou.aliyuncs.com/ceres-spring/$image_name:$date
-if [ $? -eq 0 ]
-then
-echo "tag success"
 docker push registry.cn-hangzhou.aliyuncs.com/ceres-spring/$image_name:$date
 if [ $? -eq 0 ]
 then
 echo "push success"
-docker rmi $image_name:$date
 docker rmi registry.cn-hangzhou.aliyuncs.com/ceres-spring/$image_name:$date
+kubectl set image deployment/$image_name $image_name=registry.cn-hangzhou.aliyuncs.com/ceres-spring/$image_name:$date -n spring-cloud
 else
 echo "push error"
-fi
-else
-echo "tag error"
 fi
 else
 echo "build error"
